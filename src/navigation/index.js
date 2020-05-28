bfb32a8e4f29da18ae7e42c60a7d0e4df710a68c
Ahmad Sabry <ahmadsabry330@gmail.com>
2020-05-28 08:06:39 +0200

    Fixed Deployment Bug

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,16 +3,12 @@ import { Router, Switch, Route } from "react-router-dom"
 
 import ProjectDetail from "../screens/ProjectDetail"
 import CreateProject from "../screens/CreateProject"
-import LandingPage from "../screens/LandingPage"
 import Dashboard from "../screens/Dashboard"
 import NotFound from "../screens/NotFound"
 import Profile from "../screens/Profile"
 import Builder from "../screens/Builder"
 import Viewer from "../screens/Viewer"
-// import SignIn from "../screens/SignIn"
-// import SignUp from "../screens/SignUp"
-// <Route path="/signUp" component={SignUp} /> 
-// <Route path="/signIn" component={signIn} />
+import LandingPage from "../screens/LandingPage"
 
 import NavBar from "../components/Navbar"
 import PrivateRoute from "../components/PrivateRoute"
@@ -23,15 +19,14 @@ const Navigation = () => {
     return (
         <Router history={history}>
             <NavBar />
-
             <Switch>
-                <Route exact path="/" component={LandingPage} />
                 <PrivateRoute path="/createProject" component={CreateProject} />
                 <PrivateRoute path="/project/:id" component={ProjectDetail} />
                 <PrivateRoute path="/builder/:id" component={Builder} />
                 <PrivateRoute path="/dashboard" component={Dashboard} />
                 <PrivateRoute path="/profile" component={Profile} />
                 <Route path="/viewer/:id" component={Viewer} />
+                <Route exact path="/" component={LandingPage} />
                 <Route component={NotFound} />
             </Switch>
         </Router >
