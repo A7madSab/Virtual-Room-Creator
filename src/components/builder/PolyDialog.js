962df668cd41894540f2e418816b54ae096203d9
safwansaramijou <safwan.saramijou@hotmail.com>
2020-08-11 20:26:43 +0200

    fix poly dialog style

--- src/components/builder/PolyDialog.js
+++ src/components/builder/PolyDialog.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
-import { getSearchResult } from "../../api";
-import { makeStyles, IconButton, Toolbar, AppBar, Dialog, Typography, Grid, Select, Input, MenuItem } from "@material-ui/core";
-import { CloseOutlined } from "@material-ui/icons"
+import { TextField, Select } from '@material-ui/core';
+import { makeStyles, IconButton, Toolbar, AppBar, Dialog, Typography, Grid, MenuItem } from "@material-ui/core";
 import Slide from '@material-ui/core/Slide';
 
+import { CloseOutlined } from "@material-ui/icons"
+
+import { getSearchResult } from "../../api";
 import PolyCard from "./PolyCard.js";
 
 const useStyle = makeStyles({
     appBar: {
-        backgroundColor: '#2196f3',
+        backgroundColor: '#9932CC',
         position: 'sticky'
     },
     close: {
@@ -18,13 +20,59 @@ const useStyle = makeStyles({
     title: {
         paddingLeft: '1%',
     },
-    search: {
-        margin: "15px"
+
+    inputfield: {  
+        width: '94%',
+        height: '40px',
+        color: '#eeeeee',
+        marginLeft: '6%',
+        '& label.Mui-focused': {
+            color: 'white',
+        },
+        '& label': {
+            color: 'white',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'white',
+                borderRadius: 2
+            },
+            '&:hover fieldset': {
+                borderColor: 'white',
+                borderWidth: '1px'
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: 'white',
+                borderWidth: '1px',
+            },
+        },
     },
-    select: {
-        paddingLeft: "15px",
-        width: "250px",
-        margin: "15px"
+    selectField: {  
+        color: 'white',
+        width: '50%',
+        '& label.Mui-focused': {
+            color: 'white',
+        },
+        '& label': {
+            color: 'white',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: '#9932CC',
+                borderRadius: 2,
+                color: 'white',
+            },
+            '&:hover fieldset': {
+                borderColor: '#9932CC',
+                borderWidth: '1px',
+                color: 'white',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#9932CC',
+                borderWidth: '1px',
+                color: 'white',
+            },
+        },
     },
     notFound: {
         alignItems: "center",
@@ -32,8 +80,8 @@ const useStyle = makeStyles({
         display: "flex",
         justifyContent: "center",
         justifyItems: "center",
-        width:"100%",
-        height:"80vh%"
+        width: "100%",
+        height: "80vh%"
     }
 });
 
@@ -61,16 +109,27 @@ export default function PolyDialog(props) {
                     <Typography variant="h6" className={classes.title}>
                         Poly
                     </Typography>
-                    <Input
+                    <TextField
                         fullWidth
+                        variant="outlined"
+                        size="small"
                         value={searchQuery}
-                        placeholder="Search"
-                        className={classes.search}
                         onChange={e => setSearchQuery(e.target.value)}
+                        placeholder="Search"
+                        className={classes.inputfield}
+                        InputProps={{
+                            className: classes.inputfield,
+                        }}
                     />
-                    <Select placeholder="Select Category" className={classes.select} value={searchCategory} onChange={e => setSearchCategory(e.target.value)}>
-                        <MenuItem disabled selected value="">Select Category</MenuItem>
-                        <MenuItem value="architecture">Architecture</MenuItem>
+                    <TextField select={true}  variant="outlined" value={searchCategory}
+                        onChange={e => setSearchCategory(e.target.value)}
+                        className={classes.selectField}
+                        InputProps={{
+                            className: classes.selectField,
+                        }}
+                    >
+                        <MenuItem  value="">Select Category</MenuItem>
+                        <MenuItem selected={true} value="architecture">Architecture</MenuItem>
                         <MenuItem value="animals">Animals</MenuItem>
                         <MenuItem value="art">Art</MenuItem>
                         <MenuItem value="food">Food</MenuItem>
@@ -80,11 +139,11 @@ export default function PolyDialog(props) {
                         <MenuItem value="scenes">Scenes</MenuItem>
                         <MenuItem value="technology">Technology</MenuItem>
                         <MenuItem value="transport">Transport</MenuItem>
-                    </Select>
+                    </TextField>
 
                 </Toolbar>
             </AppBar>
-            <Grid container>
+            <Grid container >
                 {searchResults
                     ? searchResults.map((model, key) => <PolyCard key={key} close={props.close} model={model} />)
                     : <Typography variant="h3" className={classes.notFound}> No Objects Found</Typography>
